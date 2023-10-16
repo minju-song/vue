bd3fcecc7c86a1fbff5fb5cd9004b45b6835a964
Song Min Ju <97097194+minju-song@users.noreply.github.com>
2023-10-16 17:50:19 +0900

    학원

---
+++ v08_data.js
@@ -0,0 +1,39 @@
+const list1 = [{
+    no: 97,
+    name: 'Kalisa Rogers',
+    tel: '010-1111',
+    address: 'Seoul'
+},
+{
+    no: 96,
+    name: 'Jesse James',
+    tel: '010-2222',
+    address: 'Seoul'
+},
+{
+    no: 95,
+    name: 'Jennifer Waler',
+    tel: '010-3333',
+    address: 'Seoul'
+}
+]
+
+const list2 = [{
+    no: 94,
+    name: 'Kalisa Rogers',
+    tel: '010-1111',
+    address: 'Seoul'
+},
+{
+    no: 93,
+    name: 'Jesse James',
+    tel: '010-2222',
+    address: 'Seoul'
+},
+{
+    no: 92,
+    name: 'Jennifer Waler',
+    tel: '010-3333',
+    address: 'Seoul'
+}
+]
